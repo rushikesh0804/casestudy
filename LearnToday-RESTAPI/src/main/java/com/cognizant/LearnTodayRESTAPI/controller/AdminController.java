37e16774efaa8944ad0be51a55dae88682fa0069
Rushi <Rushi@LAPTOP-DKVEPB49>
2021-07-12 00:09:35 +0530

    first commit uploading all files

---
+++ LearnToday-RESTAPI/src/main/java/com/cognizant/LearnTodayRESTAPI/controller/AdminController.java
@@ -0,0 +1,44 @@
+package com.cognizant.LearnTodayRESTAPI.controller;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.cognizant.LearnTodayRESTAPI.model.Course;
+import com.cognizant.LearnTodayRESTAPI.service.CourseService;
+
+@RestController
+@RequestMapping("/api/")
+public class AdminController {
+	@Autowired
+	CourseService courseService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(AdminController.class);
+	
+	@GetMapping("/admin")
+	public ResponseEntity<Object> getAllCourses(){
+		LOGGER.info("The current function is to get all courses");
+		return new ResponseEntity<>(courseService.getAllCourses(), HttpStatus.OK);
+	}
+	@GetMapping("/admin/{courseId}")
+	public ResponseEntity<Object> getCourseById(@PathVariable("courseId") int courseId){
+		LOGGER.info("The current function is to get course by id");
+		Course course = courseService.getCourseById(courseId);
+		//if(course!=null)
+			//return new ResponseEntity<>(course, HttpStatus.OK);
+			Optional<Course> checkNull=Optional.ofNullable(course);
+		if(checkNull.isPresent())
+		{
+			return new ResponseEntity<>(course, HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Searched Data Not Found", HttpStatus.NOT_FOUND);
+	}
+	
+}
---
+++ LearnToday-RESTAPI/src/main/java/com/cognizant/LearnTodayRESTAPI/controller/StudentController.java
@@ -0,0 +1,48 @@
+package com.cognizant.LearnTodayRESTAPI.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.LearnTodayRESTAPI.model.Student;
+import com.cognizant.LearnTodayRESTAPI.service.StudentService;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+	
+	@Autowired
+	StudentService studentService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(TrainerController.class);
+	@GetMapping("/student")
+	public ResponseEntity<Object> getAllCourses(){
+		LOGGER.info("The Current function is used to get the course details");
+		
+		return new ResponseEntity<>(studentService.getAllCourses(), HttpStatus.OK);
+	}
+
+	@PostMapping("/student")
+	public ResponseEntity<Object> postStudent(@RequestBody Student s){
+		LOGGER.info("The current function will post the student details in the table");
+		if(studentService.postStudent(s))
+			return new ResponseEntity<Object>( HttpStatus.CREATED);
+		return new ResponseEntity<Object>( HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	@DeleteMapping("/student/{id}")
+	public ResponseEntity<Object> deleteStudentEnrollment(@PathVariable int id) {
+		LOGGER.info("The current function will delete the student given the student id");
+			if(studentService.deleteStudent(id))
+				return new ResponseEntity<Object>( HttpStatus.OK);
+		return new ResponseEntity<Object>("No enrollment information found", HttpStatus.NOT_FOUND);
+	}
+}
